a98d4f0389f39c9124b7cf90712b1252b00f0c9d
warren-glitch <warrenpiano24@gmail.com>
2024-11-04 19:47:30 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Warren M. Piano";
+let age = "19";
+let favoriteNumber = "04";
+let favoriteColor = "white";
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number":, favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
